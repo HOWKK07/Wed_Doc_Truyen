9e85a4ba6f1d1d90025a49a1b51b1dcb2e395285
NguyenThinh <thinhnguyenkk07@gmail.com>
2025-05-13 07:05:17 +0700

    Update lại vài chỗ bị lỗi

--- wedtruyen/app/controllers/anhChuongController.php
+++ wedtruyen/app/controllers/anhChuongController.php
@@ -46,5 +46,44 @@ class AnhChuongController {
             }
         }
     }
+
+    public function themNhieuAnh(): void {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id_chuong = isset($_POST['id_chuong']) ? (int)$_POST['id_chuong'] : null;
+
+            if (!$id_chuong) {
+                throw new Exception("ID chương không được cung cấp.");
+            }
+
+            if (!isset($_FILES['anh']) || !is_array($_FILES['anh']['name'])) {
+                throw new Exception("Không có ảnh nào được tải lên.");
+            }
+
+            $target_dir = __DIR__ . "/../../../uploads/anhchuong/";
+            if (!is_dir($target_dir)) {
+                mkdir($target_dir, 0777, true);
+            }
+
+            foreach ($_FILES['anh']['name'] as $index => $name) {
+                if ($_FILES['anh']['error'][$index] === UPLOAD_ERR_OK) {
+                    $file_extension = pathinfo($name, PATHINFO_EXTENSION);
+                    $file_name = uniqid() . '.' . $file_extension;
+                    $file_path = $target_dir . $file_name;
+
+                    if (move_uploaded_file($_FILES['anh']['tmp_name'][$index], $file_path)) {
+                        $duong_dan_anh = "uploads/anhchuong/" . $file_name;
+                        $so_trang = isset($_POST['so_trang'][$index]) ? (int)$_POST['so_trang'][$index] : 0;
+
+                        $this->model->themAnh($id_chuong, $duong_dan_anh, $so_trang);
+                    } else {
+                        throw new Exception("Không thể tải lên ảnh: $name");
+                    }
+                }
+            }
+
+            header("Location: list.php?id_chuong=$id_chuong");
+            exit();
+        }
+    }
 }
 ?>
